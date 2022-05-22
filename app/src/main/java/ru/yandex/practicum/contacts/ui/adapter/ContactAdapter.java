0f81ca0d87a46a577ec0b685e237548b4e91dc2b
Vitalii Markus <vitalii.markus@gmail.com>
2022-05-22 23:09:03 +0200

    Update colors

--- app/src/main/java/ru/yandex/practicum/contacts/ui/adapter/ContactAdapter.java
+++ app/src/main/java/ru/yandex/practicum/contacts/ui/adapter/ContactAdapter.java
@@ -1,11 +1,14 @@
 package ru.yandex.practicum.contacts.ui.adapter;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.AdapterListUpdateCallback;
 import androidx.recyclerview.widget.AsyncDifferConfig;
 import androidx.recyclerview.widget.AsyncListDiffer;
@@ -15,6 +18,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 
 import java.util.List;
+import java.util.Objects;
 
 import ru.yandex.practicum.contacts.R;
 import ru.yandex.practicum.contacts.databinding.ItemContactBinding;
@@ -56,24 +60,32 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
         public ViewHolder(@NonNull ItemContactBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
-            binding.getRoot().setOnClickListener(view -> { });
+            binding.getRoot().setOnClickListener(view -> {
+            });
         }
 
         public void bind(ContactUi contact) {
             binding.name.setText(contact.getName());
-            Glide.with(binding.contactPhoto)
-                    .load(contact.getPhoto())
-                    .circleCrop()
-                    .placeholder(R.drawable.ic_avatar)
-                    .fallback(R.drawable.ic_avatar)
-                    .error(R.drawable.ic_avatar)
-                    .into(binding.contactPhoto);
+            loadAvatar(contact);
 
             final int phoneVisibility = TextUtils.isEmpty(contact.getPhone()) ? View.GONE : View.VISIBLE;
             binding.phone.setText(contact.getPhone());
             binding.phone.setVisibility(phoneVisibility);
 
             binding.contactType.setData(contact.getTypes());
+        }
+
+        private void loadAvatar(ContactUi contact) {
+            final Context context = binding.contactPhoto.getContext();
+            final Drawable drawable = Objects.requireNonNull(ContextCompat.getDrawable(context, R.drawable.ic_avatar));
+            drawable.setTint(ContextCompat.getColor(context, R.color.color_light_grey));
+            Glide.with(binding.contactPhoto)
+                    .load(contact.getPhoto())
+                    .circleCrop()
+                    .placeholder(drawable)
+                    .fallback(drawable)
+                    .error(drawable)
+                    .into(binding.contactPhoto);
         }
     }
 
